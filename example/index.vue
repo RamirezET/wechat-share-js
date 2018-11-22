942f95bfbce5f957e281f81e66a36166baf8d43b
nixin3228036@gmail.com <nixin3228036@gmail.com>
2018-11-22 10:20:14 +0800

    updating example

--- example/index.vue
+++ example/index.vue
@@ -3,3 +3,31 @@
       测试
   </div>
 </template>
+
+<script>
+import { initShare } from '@/utils/wechatShare.js'
+
+export default {
+  name: 'index',
+  data() {
+    return {
+    }
+  },
+  mounted: function() {
+    const shareData = {
+      title: '大视VLOG',
+      desc: '全民VLOG神器,遇见有趣的灵魂,用视频记录你的故事',
+      link: location.href,
+      imgUrl: 'https://ossvlog.dashimedia.cn/prod/images/logo/logo.png',
+      signUrl: encodeURIComponent(location.href)
+    }
+    initShare(shareData)
+  },
+  methods: {
+  },
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+
+</style>
